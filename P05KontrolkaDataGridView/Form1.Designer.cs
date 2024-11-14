71ec661f01fd830a7347b8ba27ea70c2ca153f1f
kurs <kurs@DESKTOP-284UO99>
2024-11-14 13:55:09 +0100

    q

---
+++ P05KontrolkaDataGridView/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace P05KontrolkaDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDane = new System.Windows.Forms.DataGridView();
+            this.txtParametryPolaczenia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPolecenieSQL = new System.Windows.Forms.TextBox();
+            this.btnWyslij = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDane)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDane
+            // 
+            this.dgvDane.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDane.Location = new System.Drawing.Point(12, 75);
+            this.dgvDane.Name = "dgvDane";
+            this.dgvDane.Size = new System.Drawing.Size(776, 363);
+            this.dgvDane.TabIndex = 0;
+            // 
+            // txtParametryPolaczenia
+            // 
+            this.txtParametryPolaczenia.Location = new System.Drawing.Point(129, 16);
+            this.txtParametryPolaczenia.Name = "txtParametryPolaczenia";
+            this.txtParametryPolaczenia.Size = new System.Drawing.Size(659, 20);
+            this.txtParametryPolaczenia.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Parametry połączenia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Polecenie SQL";
+            // 
+            // txtPolecenieSQL
+            // 
+            this.txtPolecenieSQL.Location = new System.Drawing.Point(129, 49);
+            this.txtPolecenieSQL.Name = "txtPolecenieSQL";
+            this.txtPolecenieSQL.Size = new System.Drawing.Size(575, 20);
+            this.txtPolecenieSQL.TabIndex = 3;
+            // 
+            // btnWyslij
+            // 
+            this.btnWyslij.Location = new System.Drawing.Point(713, 46);
+            this.btnWyslij.Name = "btnWyslij";
+            this.btnWyslij.Size = new System.Drawing.Size(75, 23);
+            this.btnWyslij.TabIndex = 5;
+            this.btnWyslij.Text = "Wyślij";
+            this.btnWyslij.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnWyslij);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPolecenieSQL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtParametryPolaczenia);
+            this.Controls.Add(this.dgvDane);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDane)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDane;
+        private System.Windows.Forms.TextBox txtParametryPolaczenia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPolecenieSQL;
+        private System.Windows.Forms.Button btnWyslij;
+    }
+}
+

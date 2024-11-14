eaf05e45e54ea1fb14a9a2801c91ae4328ad3a06
kurs <kurs@DESKTOP-284UO99>
2024-11-14 10:00:22 +0100

    q

---
+++ P02PolaczenieZBaza/PolaczenieZBaza.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02PolaczenieZBaza
+{
+    internal class PolaczenieZBaza
+    {
+        private readonly string connectionString;
+
+        public PolaczenieZBaza()
+        {
+            connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=A_Zawodnicy;Integrated Security=True"; 
+        }
+
+        public PolaczenieZBaza(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return connectionString;
+            }
+        }
+
+        public object[][] WykonajPolecenieSQL(string sql)
+        {
+            SqlConnection connection; // nazwiazywanie polaczenia z baza 
+            SqlCommand command; // przechowwanie polecen sql 
+            SqlDataReader sqlDataReader; // czytanie wynikow z bazy 
+
+            
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand(sql, connection);
+            connection.Open();
+            sqlDataReader = command.ExecuteReader();
+
+            int liczbaKolumn = sqlDataReader.FieldCount;
+            List<object[]> listaWierszy = new List<object[]>();
+
+            while (sqlDataReader.Read())
+            {
+                object[] komorki = new object[liczbaKolumn];
+                for (int i = 0; i < liczbaKolumn; i++)
+                    komorki[i] = sqlDataReader.GetValue(i);
+
+                listaWierszy.Add(komorki);
+            }
+
+            connection.Close();
+            return listaWierszy.ToArray();
+        }
+
+    }
+}
---
+++ P02PolaczenieZBaza/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02PolaczenieZBaza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PolaczenieZBaza pzb = new PolaczenieZBaza();
+            object[][] wynik = pzb.WykonajPolecenieSQL("select * from zawodnicy");
+
+            foreach (object[] wiersz in wynik)
+                Console.WriteLine(string.Join(" ",wiersz));
+
+
+            Console.ReadLine();
+        }
+    }
+}
